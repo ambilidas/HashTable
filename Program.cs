07e1372a9000dfa99976be4a365d9668b41fc0bb
Ambili das v s <ambili.kan@gmail.com>
2022-05-30 19:14:30 +0530

    Hash table created for large paragraphs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,29 @@ namespace HashTable
             hash.Add("5", "be");
             string hash5 = hash.Get("5");
             Console.WriteLine("5th index value: " + hash5);
+            Console.WriteLine("-----------------------------");
+            MyMapNode<string,string> myMapNode = new MyMapNode<string,string>(19);
+            myMapNode.Add("0", "paranoids");
+            myMapNode.Add("1", "are");
+            myMapNode.Add("2", "not");
+            myMapNode.Add("3", "paranoid");
+            myMapNode.Add("4", "because");
+            myMapNode.Add("5", "they");
+            myMapNode.Add("6", "are");
+            myMapNode.Add("7", "paranoid");
+            myMapNode.Add("8", "but");
+            myMapNode.Add("9", "because");
+            myMapNode.Add("10", "they");
+            myMapNode.Add("11", "keep");
+            myMapNode.Add("12", "putting");
+            myMapNode.Add("13", "themselves");
+            myMapNode.Add("14", "deliberately");
+            myMapNode.Add("15", "into");
+            myMapNode.Add("16", "paranoid");
+            myMapNode.Add("17", "avoidable");
+            myMapNode.Add("18", "situations");
+            string hash14 = myMapNode.Get("14");
+            Console.WriteLine("14th index value: " + hash14);
             Console.ReadKey();
         }
     }
